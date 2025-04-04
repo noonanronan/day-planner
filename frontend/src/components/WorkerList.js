59c26c7841a60c659825445a7cc979dfd14f37b8
Ronan Noonan <114731040+noonanronan@users.noreply.github.com>
2025-04-04 13:14:51 +0100

    Update WorkerList.js

--- frontend/src/components/WorkerList.js
+++ frontend/src/components/WorkerList.js
@@ -47,7 +47,7 @@ const WorkerList = () => {
     
         const fetchTemplates = async () => {
             try {
-                const response = await axios.get("http://127.0.0.1:5001/list-templates");
+                const response = await axios.get("https://ronannoonan.pythonanywhere.com/list-templates");
                 setTemplates(response.data.templates);
             } catch (error) {
                 console.error("Error fetching templates:", error);
